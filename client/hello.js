4b6b14788a0096c4aebe5c2d0f8e20c492b7d593
Brian Franco <mastastealth+git@gmail.com>
2015-01-05 18:26:38 -0500

    Old Syntax Example

--- client/hello.js
+++ client/hello.js
@@ -1,5 +1,11 @@
+// This doesn't work
 Template.hello.helpers = ({
 	helloWorld: function() {
 		return "HELLO WORLD";
 	}
 });
+
+// This does
+// Template.hello.helloWorld = function () {
+	// return "HELLO WORLD";
+// };
